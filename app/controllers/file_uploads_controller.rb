cf5e050f77d42b5c452cfc89bf06aa837520e952
Jack Parsons <jackp@twinsunsolutions.com>
2021-06-25 17:06:17 -0500

    added create endpoint to controller in order to submit form, working on logic for accessing the request generated by the form submission so that a failure due to timeout can be used to implement new behavior

--- app/controllers/file_uploads_controller.rb
+++ app/controllers/file_uploads_controller.rb
@@ -1,4 +1,10 @@
 class FileUploadsController < ApplicationController
   def index
   end
+
+  def create
+    pp params
+
+    redirect_to file_uploads_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/file-uploads", to: "file_uploads#index"
+  post "/file-uploads", to: "file_uploads#create"
 end
